6d32923ae65e28d6b1866497c931578757004fe7
Krishna Tiwari <krishnatiwari9191@gmail.com>
2022-05-03 11:24:49 +0200

    set mongodb connection

--- common/app_initialization.py
+++ common/app_initialization.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from common.environ import environ
+from extensions import mongo
+
+
+db_password = environ["DB_PASSWORD"]
+db_name = environ["DB_NAME"]
 
 
 def create_app():
@@ -10,5 +15,9 @@ def create_app():
 
     if environ['ENV'] == 'TEST':
         app.config.from_object('config.ConfigTesting')
+
+    app.config["MONGO_URI"] = "mongodb+srv://krishna:db_password@cluster0.7oic7.mongodb.net/db_name?retryWrites=true&w=majority"
+
+    mongo.init_app(app)
 
     return app
---
+++ extensions.py
@@ -0,0 +1,5 @@
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
+
+
