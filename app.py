239acfee326ea45c2ef37101ed44552b896fdb1e
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-05-18 20:46:34 +0200

    integrate docker

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 flask = Flask(__name__)
 
-if __name__ == "__main__":
-    flask.run()
+
+@flask.get("/")
+def home():
+    return {"message": "hello"}
+
+
+# if __name__ == "__main__":
+#     flask.run(port=8080)
