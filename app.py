2b4dfba2fb24df0ce301b0d69fe45fa6415f5ace
Rajesh Khadka <rajesh.k.khadka@gmail.com>
2022-05-30 22:56:09 +0200

    setup docker

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
+import os
+
 from flask import Flask
+from mongoengine import *
 
-flask = Flask(__name__)
+from database import db
 
 
-@flask.get("/")
+def create_app():
+    app = Flask(__name__)
+    print(os.environ.get(''))
+    app.config['MONGODB_SETTINGS'] = {
+        'host': f"mongodb://"
+                f"{os.environ.get('DB_USER')}"
+                f":{os.environ.get('DB_PASSWORD')}"
+                f"@{os.environ.get('DB_HOST')}"
+                f":{os.environ.get('DB_PORT')}"
+                f"/{os.environ.get('DB_NAME')}?authSource=admin"
+    }
+    db.init_app(app)
+    return app
+
+
+app = create_app()
+
+
+@app.get("/")
 def home():
-    return {"message": "hello"}
+    return {"hello": "world"}
 
 
-# if __name__ == "__main__":
-#     flask.run(port=8080)
+class User(Document):
+    first_name = StringField(required=True)
+    middle_name = StringField(required=True)
+    last_name = StringField(required=True)
+    email = EmailField(required=True)
+
+
+@app.post("/users")
+def create_user():
+    user = User(first_name="Rajesh", middle_name="Kumar", last_name="Khadka", email="rajesh-kumar.khadka@epita.fr")
+    user.save()
+    return {"success": True}
+
+
+@app.get("/users")
+def list_users():
+    return User.objects.to_json()
