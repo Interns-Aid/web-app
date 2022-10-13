0ed7c6d339bc33c88c88d616b2bcb3a0f90dc0f2
rajesh <rajesh.k.khadka@gmail.com>
2022-10-13 15:45:37 +0200

    add models in admin view

--- admin/__init__.py
+++ admin/__init__.py
@@ -2,7 +2,15 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 from core.extensions import db
+from models.company import Company
+from models.internship import Internship, Assignment, Home, Coding, Question
 from models.user import User
 
-admin = Admin(name='InternsAid')
+admin = Admin(name='InternsAid', template_mode='bootstrap4')
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Company, db.session))
+admin.add_view(ModelView(Internship, db.session))
+admin.add_view(ModelView(Assignment, db.session))
+admin.add_view(ModelView(Home, db.session))
+admin.add_view(ModelView(Coding, db.session))
+admin.add_view(ModelView(Question, db.session))
